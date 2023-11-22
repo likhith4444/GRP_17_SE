24bdab8690a752998705bb24f27cdaa8748acbc4
likhith4444 <yaramalalikhithkumar@gmail.com>
2023-11-21 20:08:41 -0500

    Update SE_1q.py
    The code in the SE_1q is being updated and given the result

--- Avg-turn/SE_1q.py
+++ Avg-turn/SE_1q.py
@@ -1,54 +1,43 @@
 import json
 import matplotlib.pyplot as plt
 
-def AvgPromptCount(json_file_path,Chart_title):
+def AvgPromptCount(json_file_path, Chart_title):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
 
-  with open(json_file_path, 'r') as file:
-      data = json.load(file)
-  i=0;
-  opened = [];
-  closed=[];
-  for source in data['Sources']:
-      i=i+1;
-      if source['State']=="CLOSED":
-          for y in source['ChatgptSharing']:
-              if 'NumberOfPrompts' in y:
-                  #print(y['NumberOfPrompts'])
-                  opened.append(y['NumberOfPrompts'])
-      else:
-          for y in source['ChatgptSharing']:
-              if 'NumberOfPrompts' in y:
-                  #print(y['NumberOfPrompts'])
-                  closed.append(y['NumberOfPrompts'])
+    i = 0
+    opened = []
+    closed = []
 
+    for source in data['Sources']:
+        i = i + 1
+        if source['State'] == "CLOSED":
+            for y in source['ChatgptSharing']:
+                if 'NumberOfPrompts' in y:
+                    opened.append(y['NumberOfPrompts'])
+        else:
+            for y in source['ChatgptSharing']:
+                if 'NumberOfPrompts' in y:
+                    closed.append(y['NumberOfPrompts'])
 
-  AverageOpened=round(sum(opened)/len(opened));
-  AverageClosed=round(sum(closed)/len(closed));
-  #print(i);
-  #print('opened:',AverageOpened);
-  #print('closed:',AverageClosed);
+    AverageOpened = round(sum(opened) / len(opened))
+    AverageClosed = round(sum(closed) / len(closed))
 
-  #categories
-  categories = [];
-  categories.append('Open')
-  categories.append('Closed')
-  values = [];
-  values.append(AverageOpened);
-  values.append(AverageClosed);
+    # categories
+    categories = ['Open', 'Closed']
+    values = [AverageOpened, AverageClosed]
 
+    plt.plot(categories, values, marker='o', linestyle='-', color='b')
 
-  plt.bar(categories, values)
+    # Adding labels and title
+    plt.xlabel('Issue State')
+    plt.ylabel('Count')
+    plt.title(Chart_title)
 
-  # Adding labels and title
-  plt.xlabel('Issue State')
-  plt.ylabel('Count')
-  plt.title(Chart_title)
+    # Show the line plot
+    plt.show()
 
-  # Show the bar chart
-  plt.show()
-
-
-json_file_path = 'C:\Users\yaram\Downloads\DevGPT\snapshot_20230831\20230831_060603_pr_sharings.json'
-
-AvgPromptCount('C:\Users\yaram\Downloads\DevGPT\snapshot_20230831\20230831_060603_pr_sharings.json','Pull Request')
-AvgPromptCount('C:\Users\yaram\Downloads\DevGPT\snapshot_20230831\20230831_061759_issue_sharings.json','Issue')
+json_file_path_pr = '/content/DevGPT/snapshot_20230831/20230831_060603_pr_sharings.json'
+json_file_path_issue = '/content/DevGPT/snapshot_20230831/20230831_061759_issue_sharings.json'
+AvgPromptCount(json_file_path_pr, 'Pull Request')
+AvgPromptCount(json_file_path_issue, 'Issue')
